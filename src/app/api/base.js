5570951293133630e3ebda5343ff95858d656c95
PTrabajos <20190895@aloe.ulima.edu.pe>
2023-07-21 08:43:20 -0500

    Piero

--- src/app/api/base.js
+++ src/app/api/base.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const  URI = 'entrega-prograweb-202301-api.azurewebsites.net';
+const  URI = 'https://entrega-prograweb-202301-api.azurewebsites.net';
 
 const get = async (endpoint) => {
     try {
