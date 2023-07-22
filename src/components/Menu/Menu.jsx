64c81c2c0e69327abedcbbbe83866bf65d83dc8f
DarkSide0801 <103540126+DarkSide0801@users.noreply.github.com>
2023-07-22 01:42:15 -0500

    Update Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,27 @@
-import Nav from 'react-bootstrap/Nav';
+import Link from "next/link"
+import styles from "./Menu.module.css"
 
-function Menu() {
+const Menu = () => {
   return (
-    <Nav defaultActiveKey="/home" className="flex-column">
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav>
+    <div>
+      <div className={styles.Sidebar}>
+        {window.localStorage.getItem("rol")==="profesor"?
+          <div>
+            <Link href="/UsuarioPrincipal">Principal</Link>
+            <Link href="/Perfil-Docente">Perfil</Link>
+            <Link href="/AgregarHorarios">Horarios</Link>
+            <Link href="/ResumenCalificaciones">Calificaciones</Link>
+          </div>
+        :
+          <div>
+            <Link href="/UsuarioPrincipal">Principal</Link>
+            <Link href="/Perfil-Alumno">Perfil</Link>
+            <Link href="/CitasAlumnos">Citas</Link>
+          </div>
+        }
+        <Link onClick={() => window.localStorage.setItem("recargar", "true")} href="/Login">Cerrar sesión</Link>
+      </div>
+    </div>
   );
 }
 
