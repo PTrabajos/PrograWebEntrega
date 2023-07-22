6d5af44a60d43cd91d0b89724f17ba64ecb66379
CarlosLucano <130522336+CarlosLucano@users.noreply.github.com>
2023-07-22 01:41:23 -0500

    Update page.js

--- src/app/Docente/page.js
+++ src/app/Docente/page.js
@@ -4,53 +4,103 @@ import { useEffect, useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import "./styles2.css"
 
-const Docente = () => {
 
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import styles from "@/app/page.module.css";
+
+import Inicio from "@/components/Inicio/inicio.jsx" //cambio
+
+
+const User = () => {
+    Inicio("/PantallaPrincipal");
+    const [nameUser, setNameUser] = useState("");
+
+    useEffect(() => {
+      const nameUser = window.localStorage.getItem("nameUser");
+      setNameUser(nameUser);
+    }, [])
+
+//Atención alumno
+
+const [atenciones, setAtenciones] = useState([]);
+const [pageActual, setPageActual] = useState(1);
+const atencionesPage = 4;
+const totalPages = Math.ceil(atenciones.length/ atencionesPage);
+
+useEffect(() => {
+    obteneratencionesReservadas();
+}, []);
+
+const obteneratencionesReservadas = () => {
+    const storedAtenciones = localStorage.getItem("Atenciones");
+    if (storedAtenciones){
+        const atencionesReservadas = JSON.parse(storedAtenciones);
+        setAtenciones(atencionesReservadas);
+    }
+};
+
+const cambiarPage = (page) => {
+    setPageActual(page);
+}
   
+const atencionesxPage = atenciones.slice(
+    (pageActual - 1) * atencionesxPage,
+    pageActual * atencionesxPage
+);
     return (
-        <div >
-         
+        <div>
+         {window.localStorage.getItem("rol")==="profesor"?
+             <h2>Bienvenido, Profesor(a) {nameUser}!</h2>
+             :
+             <h2>Bienvenido, {nameUser}!</h2>
+            }
         
-        <div class="middlebar">
-            <h2>BIENVENIDO, PROFESOR JUAN</h2>
-            
-        </div>
-        <hr class="linea"/>
+            <hr class="linea"/>
 
-        <div class="appointments">
-                <table className="tablaCitas"><br/>
+            <div class="appointments">
+              
                 
                     <h3> PROXIMAS CITAS</h3>
                     
-                                
-                        <br/>
-                    
-                        <button class="asesoria1" >
-                            
-                        <h4>Asesoria Redes de Computadora</h4>
-                        <p>Fecha: 15/05/2023</p>
-                        <p>Hora: 10:00 am</p>
-                        </button>
-                        <br/><br/>
-                        <button class="asesoria2" >
-                        <h4>Asesoria Programacion Web</h4>
-                        <p>Fecha: 16/05/2023</p>
-                        <p>Hora: 2:00 pm</p>
-                        </button> 
-
-                        <p class="indice">
-                            Página 1 de 1
-                        </p>
-            </table>
-        </div>
-        <div class="appointments">
-            <table className="tablaCitas">
-
-            </table>
-        </div>
-    
-    </div>
+                    <div className={styles.container}>
+                        {citasPagina.length > 0 ? (
+                          <div className = {styles.citasGrid}>
+                            {atencionesPagina.map((atencion, index) => (
+                              <div key = {index} className = {styles.atencionContainer}>
+                                <div className = {styles.numeroAtencion}>
+                                  {(pageActual -1 ) * atencionesPage + index + 1}    
+                                </div> 
+                                <div className = {styles.infoCita}>
+                                  El {atencion.dia} de {atencion.horaInicio} a {atencion.horario}
+                                </div>
+                                </div>
+                            ))}
+                        </div>
+                        ) : (
+                            <p>No hay citas reservadas</p>
+                        )}
+                        {totalPages > 1 && (
+                            <div className = {styles.pagination}>
+                                {Array.from({length: totalPages}, (_, index) => (
+                                <button
+                                    key = {index}
+                                    onClick={() => cambiarPage(index + 1)}
+                                    className={`${
+                                        pageActual === index + 1 ? styles.active:""
+                                        } ${styles.paginationButton}`}
+                                >
+                                    {index + 1}
+                                </button>
+                                ))}
+                             </div>
+                            )}
+                        </div>
+                        </div>
+                        <br/>          
+        </div>   
     )
 } 
 
-export default Docente
+export default User
+
