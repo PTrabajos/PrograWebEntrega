71a14d3b5cb259b87d9412d7a69547c31d1bcc71
DarkSide0801 <103540126+DarkSide0801@users.noreply.github.com>
2023-07-22 01:38:33 -0500

    Update TopBar.jsx

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,34 +1,36 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from "react-bootstrap/Navbar";
+import { FaUserCircle, FaBars } from "react-icons/fa";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styles from "../TopBar/TopBar.module.css";
 
-const TopBar = () => {
+const TopBar = ({ onButtonClick }) => {
+  const router = useRouter();
+
+  const rolUsuario = localStorage.getItem("rol");
+  const handleProfileClick = () => {
+    if (rolUsuario === "profesor") {
+      router.push("/Perfil-Docente");
+    } else if (rolUsuario === "estudiante") {
+      router.push("/Perfil-Alumno");
+    } else {
+      alert("Rol de usuario no válido");
+    }
+  };
   return (
-    <Navbar nameclass="color-nav" bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand class="style-nav" href="#home">Mis Citas</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar className={styles.NavbarStyle} expand="xxl">
+        <button className={styles.Btn} onClick={onButtonClick}>
+          <FaBars size={25} color="black" />
+        </button>
+        <Navbar.Brand href="#home">Atención de Citas</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+        <button className={styles.Btn} onClick={handleProfileClick}>
+          <FaUserCircle size={25} color="#684FA5" />
+        </button>
+      </Navbar>
+    </div>
   );
-}
+};
 
 export default TopBar;
